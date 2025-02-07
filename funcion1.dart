4acd0d3b2a92e7ccd0bb2ffc95de3cce5ec54a6e
Dante-Dominguez-1061 <164534070+Dante-Dominguez-1061@users.noreply.github.com>
2025-02-07 16:47:51 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Dante Dominguez');
+}
+
+void main(){
+  // Dante Dominguez Mat: 22308051281061
+  // Llamar a la funcion saludar
+  print('Dante Dominguez Mat: 22308051281061');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+}
