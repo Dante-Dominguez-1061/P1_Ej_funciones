ad527602e338d6db79a16fe33967e34c6372de7f
Dante-Dominguez-1061 <164534070+Dante-Dominguez-1061@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+// Funciones creadas por el usuario
+void suma(int a, int b) {
+  print('La suma es: ${a + b}');
+}
+
+void resta(int a, int b) {
+  print('La resta es: ${a - b}');
+}
+// Funcion main
+void main() {
+  print('Dante Dominguez Mat: 22308051281061');
+  print('Llamando a la funcion suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
