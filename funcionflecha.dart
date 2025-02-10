356729cd2385b2b89d9a09e402947417b5cc5e9c
Dante-Dominguez-1061 <164534070+Dante-Dominguez-1061@users.noreply.github.com>
2025-02-10 18:32:20 +0000

    Publique funcion flecha

---
+++ funcionflecha.dart
@@ -0,0 +1,24 @@
+int multiplicacion(int a, int b) {
+  return a * b;
+}
+
+int flechamultiplicacion(int a, int b) => a * b;
+
+double division(double a, double b) {
+  return a / b;
+}
+
+double flechadivision(double a, double b) => a / b;
+// returnType functionName(parameters...) => expression;
+// Funcion main
+void main() {
+  print('Dante Dominguez Mat: 22308051281061');
+  print('Llamando a la funcion multiplicacion');
+  print(multiplicacion(10, 5));
+  print("Llamando a la funcion flechamultiplicacion");
+  print(flechamultiplicacion(3, 5));
+  print('Llamando a la funcion division');
+  print(division(10, 3));
+  print('Llamando a la funcion flechadivision');
+  print(flechadivision(10, 3));
+}
